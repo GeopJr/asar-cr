862ec3cda9898f709feff0f753ea35fc143a8fc6
Michael Petö <michael@petoe.me>
2017-08-02 19:07:16 +0200

    Add some tests

--- spec/asar_spec.cr
+++ spec/asar_spec.cr
@@ -1,9 +1,18 @@
 require "./spec_helper"
 
 describe Asar do
-  # TODO: Write tests
+  it "get hello.txt" do
+    asar = Asar::Reader.new "spec/test/archive.asar"
+    asar.get("/hello.txt").gets_to_end.should eq "Hello Asar!\n"
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "get_bytes image.png" do
+    asar = Asar::Reader.new "spec/test/archive.asar"
+    asar.get_bytes("/assets/image.png").size.should eq 2632
+  end
+
+  it "read_raw random.css" do
+    asar = Asar::Reader.new "spec/test/archive.asar"
+    String.new(asar.read_raw("/assets/random.css")).should eq `cat spec/test/archive/assets/random.css`
   end
 end
