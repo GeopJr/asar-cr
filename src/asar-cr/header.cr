d7ac76ed1f4b2cfa523329e276a196e1cd565ed9
GeopJr <geopjr@geopjr.xyz>
2020-12-08 18:56:20 +0200

    :arrow_up: Hello 0.35.1

--- src/asar-cr/header.cr
+++ src/asar-cr/header.cr
@@ -1,37 +1,37 @@
 require "json"
 
 module Asar
-  # The same as `Dir` but it represents the top level directory of the asar archive.
+  # :nodoc:
   alias Archive = Directory
 
-  # A directory contains other `Dir` or `File` objects.
+  # A directory contains other `Directory`, `File` or `Link` objects.
   class Directory
-    JSON.mapping(
-      files: Hash(String, Directory | File | Link)
-    )
+    include JSON::Serializable
+
+    property files : Hash(String, Directory | File | Link)
   end
 
   # A `File` contains the size, offset and executable flag stored inside the archive header.
   class File
-    JSON.mapping(
-      size: Int32,
-      offset: String,
-      executable: Bool?
-    )
+    include JSON::Serializable
+
+    property size : Int32
+    property offset : String
+    property executable : Bool?
   end
 
   # A `Link` contains the link flag stored inside the archive header.
   class Link
-    JSON.mapping(
-      link: String
-    )
+    include JSON::Serializable
+
+    property link : String
   end
 
   # `Header` contains the asar archive header data and length.
   struct Header
     # Header length needed for offset calculation.
     getter length : Int32
-    # Archive that contains `File` and `Dir` information.
+    # Archive that contains `File` and `Directory` information.
     getter data : Archive
 
     def initialize(@data : Archive, @length : Int32)
